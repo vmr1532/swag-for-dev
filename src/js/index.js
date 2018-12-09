0e9faf23e3d8c7158fdff2508b84e44c114e81e3
Mihir Chaturvedi <plibither8@gmail.com>
2018-12-09 11:20:04 +0530

    feat: Add "Date added" as a sorting method

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,9 @@ const sort = {
 	ALPHABETICAL_ASCENDING: (a, b) => a.dataset.name > b.dataset.name ? 1 : -1,
 	ALPHABETICAL_DESCENDING: (a, b) => a.dataset.name < b.dataset.name ? 1 : -1,
 	DIFFICULTY_ASCENDING: (a, b) => a.dataset.difficulty > b.dataset.difficulty ? 1 : -1,
-	DIFFICULTY_DESCENDING: (a, b) => a.dataset.difficulty < b.dataset.difficulty ? 1 : -1
+	DIFFICULTY_DESCENDING: (a, b) => a.dataset.difficulty < b.dataset.difficulty ? 1 : -1,
+	DATEADDED_ASCENDING: (a, b) => (new Date(a.dataset.dateadded)).getTime() > (new Date(b.dataset.dateadded)).getTime() ? 1 : -1,
+	DATEADDED_DESCENDING: (a, b) => (new Date(a.dataset.dateadded)).getTime() < (new Date(b.dataset.dateadded)).getTime() ? 1 : -1
 };
 
 const contentEl = document.getElementById('content');
